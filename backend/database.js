7651f3b6c60d4df6d27e6286b9251fdfcef85287
s-almeda <shm.almeda@berkeley.edu>
2025-03-12 02:36:04 -0700

    correct capitalization?

--- backend/database.js
+++ backend/database.js
@@ -2,7 +2,7 @@ import sqlite3 from "sqlite3";
 import { open } from "sqlite";
 import fs from 'fs';
 
-const defaultCanvasData = JSON.parse(fs.readFileSync('./defaultcanvasdata.json', 'utf-8'));
+const defaultCanvasData = JSON.parse(fs.readFileSync('./defaultCanvasData.json', 'utf-8'));
 const dbPromise = open({
   filename: "./database.db",
   driver: sqlite3.Database,
